fa9874863f1a37a27b860d9ddfa3bd273af1a917
unusedChar63 <41201591+unusedChar63@users.noreply.github.com>
2018-07-16 14:22:39 +0600

    ok

---
+++ 1st Day/sizeof.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Storage size for int: %ld\n",sizeof(int));
+    printf("Storage size for int: %ld\n",sizeof(long));
+    printf("Storage size for int: %ld\n",sizeof(char));
+    printf("Storage size for int: %ld\n",sizeof(double));
+    printf("Storage size for int: %ld\n",sizeof(float));
+    return 0;
+}
